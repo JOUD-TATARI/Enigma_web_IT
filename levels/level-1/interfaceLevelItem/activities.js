dcf05510bf21014e8744eca05514f6e5291533b2
JOUD-TATARI <149987332+JOUD-TATARI@users.noreply.github.com>
2024-03-03 10:26:05 +0300

    Update activities.js

--- levels/level-1/interfaceLevelItem/activities.js
+++ levels/level-1/interfaceLevelItem/activities.js
@@ -3,3 +3,33 @@ window.scrollTo({
     left:100,
     behavior:"smooth"
 });
+document.querySelector(".header-bar").onclick=function()
+{
+ 
+  document.querySelector(".list-bar").style.display="block";
+  document.querySelector(".list-bar").style.zIndex="1000000000000";
+   document.querySelector(".before-home").style.display="none";
+
+
+  
+}
+document.querySelector(".div2-list-bar-levels-icon .hide").onclick=function()
+{
+
+    document.querySelector(".div2-list-bar-levels-hidden").style.display="block"
+    document.querySelector(".div2-list-bar-levels-icon .hide").style.display="none"
+    document.querySelector(".div2-list-bar-levels-icon .visible").style.display="inline"
+  
+}
+document.querySelector(".div2-list-bar-levels-icon .visible").onclick=function()
+{
+   document.querySelector(".div2-list-bar-levels-hidden").style.display="none"
+    document.querySelector(".div2-list-bar-levels-icon .hide").style.display="inline"
+    document.querySelector(".div2-list-bar-levels-icon .visible").style.display="none"
+}
+document.querySelector(".div1-list-bar").onclick=function()
+{
+  document.querySelector(".list-bar").style.display="none"
+  document.querySelector(".list-bar").style.zIndex="-100"
+  document.querySelector(".before-home").style.display="block";
+}
