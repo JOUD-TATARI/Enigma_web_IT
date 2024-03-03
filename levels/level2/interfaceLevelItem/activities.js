9ab15501902bf4b63301e701aa8e20aaae0eab65
JOUD-TATARI <149987332+JOUD-TATARI@users.noreply.github.com>
2024-03-03 10:40:19 +0300

    Update activities.js

--- levels/level2/interfaceLevelItem/activities.js
+++ levels/level2/interfaceLevelItem/activities.js
@@ -4,10 +4,15 @@ window.scrollTo({
     behavior:"smooth"
 });
 
+
 document.querySelector(".header-bar").onclick=function()
 {
-  document.querySelector(".list-bar").style.display="block"
+ 
+  document.querySelector(".list-bar").style.display="block";
   document.querySelector(".list-bar").style.zIndex="1000000000000";
+   document.querySelector(".before-home").style.display="none";
+
+
   
 }
 document.querySelector(".div2-list-bar-levels-icon .hide").onclick=function()
@@ -28,4 +33,5 @@ document.querySelector(".div1-list-bar").onclick=function()
 {
   document.querySelector(".list-bar").style.display="none"
   document.querySelector(".list-bar").style.zIndex="-100"
+  document.querySelector(".before-home").style.display="block";
 }
