83b1078d4001b63f4cbd1806b8144d23faab4840
JOUD-TATARI <149987332+JOUD-TATARI@users.noreply.github.com>
2024-03-03 10:13:25 +0300

    Update lesson3.js

--- levels/level-1/interfaceLesson/lessons/lesson3.js
+++ levels/level-1/interfaceLesson/lessons/lesson3.js
@@ -80,8 +80,12 @@
      
 document.querySelector(".header-bar").onclick=function()
 {
-  document.querySelector(".list-bar").style.display="block"
+ 
+  document.querySelector(".list-bar").style.display="block";
   document.querySelector(".list-bar").style.zIndex="1000000000000";
+   document.querySelector(".before-home").style.display="none";
+
+
   
 }
 document.querySelector(".div2-list-bar-levels-icon .hide").onclick=function()
@@ -101,5 +105,6 @@ document.querySelector(".div2-list-bar-levels-icon .visible").onclick=function()
 document.querySelector(".div1-list-bar").onclick=function()
 {
   document.querySelector(".list-bar").style.display="none"
-  document.querySelector(".list-bar").style.zIndex="-100";
+  document.querySelector(".list-bar").style.zIndex="-100"
+  document.querySelector(".before-home").style.display="block";
 }
